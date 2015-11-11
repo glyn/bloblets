b71a54af48f082506f7fbde5e9d1a56c2d9f2e20
Glyn Normington <gnormington@pivotal.io>
2015-11-11 09:43:55 +0000

    appbits resources demarshalling

--- appbits/abserver.go
+++ appbits/abserver.go
@@ -1,10 +1,27 @@
 package appbits
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+
+	"github.com/cloudfoundry/cli/cf/api/resources"
+	"github.com/glyn/bloblets/servutil"
 )
 
 func AppHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("appbits.AppHandler not implemented")
+	r.ParseMultipartForm(100 * 1024 * 1024)
+	mpForm := r.MultipartForm
+
+	res := mpForm.Value["resources"]
+	presentFiles := []resources.AppFileResource{}
+	err := json.Unmarshal([]byte(res[0]), &presentFiles)
+	if err != nil {
+		servutil.Fail(w, "demarshalling resources failed: %s", err)
+		return
+	}
+
+	application := mpForm.File["application"]
+
+	fmt.Printf("appbits.AppHandler not implemented:\npresentFiles=%#v\napplication=%#v\n", presentFiles, application)
 }
