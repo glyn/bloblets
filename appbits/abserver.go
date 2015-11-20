eca8b369879594567b76edc46b835a7eff0da77b
Glyn Normington <gnormington@pivotal.io>
2015-11-20 12:00:03 +0000

    Cope when application.zip is not uploaded
    Also ensure directories are created for files downloaded from the
    blobstore.

--- appbits/abserver.go
+++ appbits/abserver.go
@@ -33,67 +33,70 @@ func AppHandler(w http.ResponseWriter, r *http.Request) {
 	r.ParseMultipartForm(100 * 1024 * 1024)
 	mpForm := r.MultipartForm
 
-	// Unzip the uploaded portion of the application.
-	application := mpForm.File["application"][0]
-	zipFile, err := application.Open()
-	if err != nil {
-		servutil.Fail(w, "failed to open application zip: %s", err)
-		return
-	}
-	cl := application.Header.Get("Content-Length")
-	if cl == "" {
-		servutil.Fail(w, "Content-Length header not supplied for application zip: %s", err)
-		return
-	}
-	zipLen, err := strconv.ParseInt(cl, 10, 64)
-	if err != nil {
-		servutil.Fail(w, "Invalid Content-Length %s: %s", cl, err)
-		return
-	}
-
-	zr, err := zip.NewReader(zipFile, zipLen)
-
-	for _, f := range zr.File {
-		destPath := filepath.Join(appDir, f.Name)
-		destDir, _ := filepath.Split(destPath)
-		err := os.MkdirAll(destDir[:len(destDir)-1], 0755)
+	// Unzip any uploaded portion of the application.
+	appHdrs, ok := mpForm.File["application"]
+	if ok {
+		application := appHdrs[0]
+		zipFile, err := application.Open()
 		if err != nil {
-			servutil.Fail(w, "Failed to create destination directory: %s", err)
+			servutil.Fail(w, "failed to open application zip: %s", err)
+			return
+		}
+		cl := application.Header.Get("Content-Length")
+		if cl == "" {
+			servutil.Fail(w, "Content-Length header not supplied for application zip: %s", err)
+			return
+		}
+		zipLen, err := strconv.ParseInt(cl, 10, 64)
+		if err != nil {
+			servutil.Fail(w, "Invalid Content-Length %s: %s", cl, err)
 			return
 		}
 
-		if f.FileInfo().IsDir() {
-			continue
-		}
+		zr, err := zip.NewReader(zipFile, zipLen)
 
-		dest, err := os.OpenFile(destPath, os.O_CREATE|os.O_RDWR, 0755)
-		if err != nil {
-			servutil.Fail(w, "Failed to create destination file: %s", err)
-			return
-		}
+		for _, f := range zr.File {
+			destPath := filepath.Join(appDir, f.Name)
+			destDir, _ := filepath.Split(destPath)
+			err := os.MkdirAll(destDir[:len(destDir)-1], 0755)
+			if err != nil {
+				servutil.Fail(w, "Failed to create destination directory: %s", err)
+				return
+			}
 
-		of, err := f.Open()
-		if err != nil {
-			servutil.Fail(w, "Failed to open application file: %s", err)
-			return
-		}
+			if f.FileInfo().IsDir() {
+				continue
+			}
 
-		io.Copy(dest, of)
-		of.Close()
-		fi, err := dest.Stat()
-		if err != nil {
-			servutil.Fail(w, "Failed to stat application file: %s", err)
-			return
+			dest, err := os.OpenFile(destPath, os.O_CREATE|os.O_RDWR, 0755)
+			if err != nil {
+				servutil.Fail(w, "Failed to create destination file: %s", err)
+				return
+			}
+
+			of, err := f.Open()
+			if err != nil {
+				servutil.Fail(w, "Failed to open application file: %s", err)
+				return
+			}
+
+			io.Copy(dest, of)
+			of.Close()
+			fi, err := dest.Stat()
+			if err != nil {
+				servutil.Fail(w, "Failed to stat application file: %s", err)
+				return
+			}
+			dest.Close()
+			fn := dest.Name()
+			if !fi.IsDir() && fi.Size() > 65535 {
+				sha := sha(fn)
+				log.Printf("AppHandler adding file %s to the blob store: %s\n", f.Name, sha)
+				blobstore.Add(sha, fn)
+			}
 		}
-		dest.Close()
-		fn := dest.Name()
-		if !fi.IsDir() && fi.Size() > 65535 {
-			sha := sha(fn)
-			log.Printf("AppHandler adding file %s to the blob store: %s\n", f.Name, sha)
-			blobstore.Add(sha, fn)
-		}
+		log.Println("AppHandler unzipped uploaded portion of application")
 	}
-	log.Println("AppHandler unzipped uploaded portion of application")
 
 	// Add blobs to the application
 	res := mpForm.Value["resources"]
@@ -105,6 +108,12 @@ func AppHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	for _, pf := range presentFiles {
 		dest := filepath.Join(appDir, pf.Path)
+		destDir, _ := filepath.Split(dest)
+		err = os.MkdirAll(destDir, 0755)
+		if err != nil {
+			servutil.Fail(w, "creating destination directory for downloaded file failed: %s", err)
+			return
+		}
 		blobstore.Get(pf.Sha1, dest)
 	}
 
